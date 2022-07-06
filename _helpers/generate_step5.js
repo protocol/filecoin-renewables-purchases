3338b03e8d7e3269ece0195875ef509e17c3f523
Red <aransil@gmail.com>
2022-07-06 02:42:44 +0200

    new step5 generation code

---
+++ _helpers/generate_step5.js
@@ -0,0 +1,103 @@
+let fs = require('fs');
+const { parse } = require('csv-parse/sync');
+const json2csvparse = require('json2csv');
+
+// All automatic redemptions right now are in China, so this code assumes supplier and beneficiary countries match
+function gen_step_5_auto(transaction_folder, contracts, smart_contract_id, attestation_folder){
+
+  // Load data for steps 2 and 3, and the manual info for step5
+  const step2_file=transaction_folder+'_step2_orderSupply.csv'
+  const step3_file=transaction_folder+'_step3_match.csv'
+
+  const step2_data = parse(fs.readFileSync(transaction_folder+'/'+step2_file, {encoding:'utf8', flag:'r'}), {columns: true, cast: true});
+  const step3_data = parse(fs.readFileSync(transaction_folder+'/'+step3_file, {encoding:'utf8', flag:'r'}), {columns: true, cast: true});
+
+  // We need to know the batch number that's already been allocated.
+  // Do this by first loading all of the other allocations
+  // For this import, have to turn cast off so address reads as a string
+  const transaction_array = parse(fs.readFileSync('all_transactions.csv', {encoding:'utf8', flag:'r'}), {columns: true, cast: true});
+  var all_allocations = []
+  transaction_array.forEach(function(transaction, idx){
+    // console.log(transaction.transaction_folder)
+    try{
+      newData = parse(fs.readFileSync(transaction.transaction_folder+'/'+transaction.transaction_folder+'_step5_redemption_information_manual.csv', {encoding:'utf8', flag:'r'}), {columns: true, cast: false});
+      // console.log(`Found ${transaction.transaction_folder+'_step5_redemption_information_manual.csv'}`)
+      all_allocations = all_allocations.concat(newData)}catch{}
+    try{
+      newData = parse(fs.readFileSync(transaction.transaction_folder+'/'+transaction.transaction_folder+'_step5_redemption_information_automatic.csv', {encoding:'utf8', flag:'r'}), {columns: true, cast: false});
+      // console.log(`Found ${transaction.transaction_folder+'_step5_redemption_information_automatic.csv'}`)
+      // If this is the file we are making (ie we ran this script multiple times), ignore it
+      if (!(transaction.transaction_folder == transaction_folder)){all_allocations = all_allocations.concat(newData)}
+    }catch{}
+  })
+  allocations_same_smart_contract_id = all_allocations.filter(x=> x.smart_contract_address == smart_contract_id)
+  allocation_numbers = allocations_same_smart_contract_id.map(x=> Number(x.batchID))
+  batch_previous_match = allocation_numbers.reduce((prev, elem)=> {if (elem>prev){return elem} else{return prev}}, 0)
+  console.log(`Maximum previous batch number found: ${batch_previous_match}`)
+
+
+  // Find the previous max attestation number
+  // We are assuming that this function generates a new automatic step 5
+  // Previous attestations for this transaction are only in the corresponding manual attestation file for this transaction
+  var prevAttestation_max = 0
+  try{
+    this_transaction_manual = parse(fs.readFileSync(transaction_folder+'/'+transaction_folder+'_step5_redemption_information_manual.csv', {encoding:'utf8', flag:'r'}), {columns: true, cast: false});
+    // console.log(`Found ${transaction_folder+'_step5_redemption_information_manual.csv'}`)
+    this_transaction_manual_attestation_numbers = this_transaction_manual.map(x=> Number(x.attestation_id.replace(transaction_folder+'_attestation_', '')))
+    prevAttestation_max = this_transaction_manual_attestation_numbers.reduce((prev, elem)=> {if(elem>prev){return elem} else{return prev}}, 0)
+  }catch{}
+  console.log(`Maximum previous attestation number found for this transaction: ${prevAttestation_max}`)
+
+  // We were passed a list of contracts to generate auto redemptions for. Loop through them
+  new_allocations = []
+  var batch_new = batch_previous_match
+  var attest_new = prevAttestation_max
+  contracts.forEach(function (contract, index) {
+
+    // console.log(contract)
+
+    // Find step 2 and step 3 data corresponding to this contract and make sure the volumes match
+    step2_thisContract = step2_data.filter(x=>x.contract_id == contract)
+    step2_thisContract = step2_thisContract[0]
+    step3_thisContract = step3_data.filter(x=>x.contract_id == contract)
+    step3_thisContract_totalVol = step3_thisContract.reduce((prev, elem) => {return prev + elem.volume_MWh}, 0)
+    if (! step3_thisContract_totalVol == step2_thisContract.volume_MWh){error('Step 3 volume doesnt match step 2')}
+
+    location = step2_thisContract.country
+    if (step2_thisContract.region.length >0){location = step2_thisContract.country + '-' +step2_thisContract.region}
+    // Make an entry for each step3 allocation
+    this_contract_allocations = step3_thisContract.map(elem => {
+      attest_new += 1
+      batch_new += 1
+      return {
+        'attestation_id': `${transaction_folder}_attestation_${attest_new}`,
+        'redemption_process': 'automatic',
+        'contract_id': contract,
+        'allocation_id': elem.allocation_id,
+        'smart_contract_address': smart_contract_id,
+        'batchID':batch_new,
+        'network': 246,
+        'zl_protocol_version': 'ZL 1.0.0',
+        'minerID': elem.minerID,
+        'beneficiary':`Blockchain Network ID: 246 - Tokenization Protocol: ZL 1.0.0 - Smart Contract Address: ${smart_contract_id} - Batch ID: ${batch_new} - Filecoin minerID ${elem.minerID}`,
+        'beneficiary_country':step2_thisContract.country,
+        'beneficiary_location':location,
+        'supply_country':step2_thisContract.country,
+        'volume_required':elem.volume_MWh,
+        'start_date':step2_thisContract.reportingStart,
+        'end_date':step2_thisContract.reportingEnd,
+        'redemption_purpose':`The certificates are redeemed (= assigned to the beneficiary) for the purpose of tokenization and bridging to the Blockchain: Energy Web Chain with the Network ID 246. The smart contract address is ${smart_contract_id} and the specific certificate batch ID is ${batch_new}. The certificates will be created as tokens of type ERC1888-Topic1 This redemption is matched to Filecoin minerID ${elem.minerID}`,
+        'attestation_folder': attestation_folder
+      }
+    })
+
+    new_allocations = new_allocations.concat(this_contract_allocations)
+
+  })
+
+  console.log(new_allocations.length)
+  fs.writeFileSync(transaction_folder+'/'+transaction_folder+'_step5_redemption_information_automatic.csv', json2csvparse.parse(new_allocations));
+}
+
+
+module.exports = {gen_step_5_auto}
---
+++ main.js
@@ -0,0 +1,49 @@
+// let catalog_renewables_purchases = require("./catalog_renewables_purchases").catalog_renewables_purchases
+// let orders = require("./orders")
+// let getEnergy = require('./getEnergy')
+let test_utilities = require('./test_utility.js')
+let gen5 = require('./_helpers/generate_step5.js').gen_step_5_auto
+
+// Run this to assess renewable energy purchases that have been associated with nodes
+// catalog_renewables_purchases()
+
+// Take supply and match to SP electricity to produce a redemption order
+// folder = '20220407_fractionalize_contract_supply'
+// orders.new_EAC_redemption_order(folder)
+// folder = 'test'
+// orders.new_EAC_redemption_order(folder)
+
+// tests = ['20220420_SP_FF_transaction_1', '20220501_ACT_PL_transaction_1', '20220607_SP_FF_transaction_2']
+//
+// test_utilities.test_step_3(tests[2])
+
+
+// 20220704: generate step 5 automatic redemptions
+const smart_contract_id = '0x2248a8E53c8cf533AEEf2369FfF9Dc8C036c8900'
+attestation_folder = '20220510_ACT_PL_transaction_1_delivery_1'
+
+ACT_PL_transaction_1_folder = '20220501_ACT_PL_transaction_1'
+ACT_PL_transaction_1_IREC_contracts = ['20220501_ACT_PL_transaction_1_contract_14', '20220501_ACT_PL_transaction_1_contract_15', '20220501_ACT_PL_transaction_1_contract_16', '20220501_ACT_PL_transaction_1_contract_17', '20220501_ACT_PL_transaction_1_contract_18']
+
+SP_FF_transaction_1_folder = '20220420_SP_FF_transaction_1'
+SP_FF_transaction_1_IREC_contracts = ['20220420_SP_FF_transaction_1_contract_1']
+
+SP_FF_transaction_2_folder = '20220607_SP_FF_transaction_2'
+SP_FF_transaction_2_IREC_contracts = ['20220607_SP_FF_transaction_2_contract_1', '20220607_SP_FF_transaction_2_contract_2', '20220607_SP_FF_transaction_2_contract_3']
+
+// gen5(ACT_PL_transaction_1_folder, ACT_PL_transaction_1_IREC_contracts, smart_contract_id, attestation_folder)
+// gen5(SP_FF_transaction_1_folder, SP_FF_transaction_1_IREC_contracts, smart_contract_id, attestation_folder)
+// gen5(SP_FF_transaction_2_folder, SP_FF_transaction_2_IREC_contracts, smart_contract_id, attestation_folder)
+
+test_utilities.test_step_5()
+
+// Run checks
+// folder = '20220318_order'
+// orders.checkOrder(folder)
+
+// async function main(){
+//   result = await getEnergy.get_total_energy_data('2020-08-01', '2022-03-20', 'f066596')
+//   console.log(result)
+// }
+//
+// main()
